c71c75ccf17a9afde147ca24c21a2d0715aabfae
Adeilson Silva <adeilson.silva@brainweb.com.br>
2023-08-15 08:55:36 -0300

    class 1

---
+++ dw_barbershop/lib/src/core/ui/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+sealed class ColorsConstants {
+  static const brow = Color(0XFFB07B01);
+  static const grey = Color(0XFF999999);
+  static const greyLigth = Color(0XFFE6E2E9);
+  static const red = Color(0XFFEB1212);
+}
+
+sealed class FontsConstants {
+  static const fontFamily = 'Poppins';
+}
