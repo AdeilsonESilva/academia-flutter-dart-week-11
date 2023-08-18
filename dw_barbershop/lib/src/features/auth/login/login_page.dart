dbb07e61d77fba2f698033ada418682d9d7bec45
Adeilson Silva <adeilson.silva@brainweb.com.br>
2023-08-18 07:32:56 -0300

    class 3

--- dw_barbershop/lib/src/features/auth/login/login_page.dart
+++ dw_barbershop/lib/src/features/auth/login/login_page.dart
@@ -123,6 +123,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                           const SizedBox(height: 24),
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
+                              // TODO interesting
                               minimumSize: const Size.fromHeight(56),
                             ),
                             onPressed: () {
@@ -140,14 +141,18 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                           ),
                         ],
                       ),
-                      const Align(
+                      Align(
                         alignment: Alignment.bottomCenter,
-                        child: Text(
-                          'Criar conta',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
+                        child: InkWell(
+                          onTap: () => Navigator.of(context)
+                              .pushNamed('/auth/register/user'),
+                          child: const Text(
+                            'Criar conta',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
---
+++ dw_barbershop/lib/src/features/auth/register/barbershop/barbershop_register_vm.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'barbershop_register_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$barbershopRegisterVmHash() =>
+    r'a18b0091f674ded10742aa7d2647a307122b5d12';
+
+/// See also [BarbershopRegisterVm].
+@ProviderFor(BarbershopRegisterVm)
+final barbershopRegisterVmProvider = AutoDisposeNotifierProvider<
+    BarbershopRegisterVm, BarbershopRegisterState>.internal(
+  BarbershopRegisterVm.new,
+  name: r'barbershopRegisterVmProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$barbershopRegisterVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BarbershopRegisterVm = AutoDisposeNotifier<BarbershopRegisterState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
